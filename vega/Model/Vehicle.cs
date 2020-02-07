9e3f777c317b8f0248645084f2c1892de9973298
Rafael Silva <rafaelsilvarnj@gmail.com>
2020-02-06 21:56:37 -0300

    Added Vehicle Model

---
+++ vega/Model/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vega.Model
+{
+    [Table("Vehicles")]
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+        public bool IsRegistered { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string ContactName { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string ContactPhone { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string ContactEmail { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}
---
+++ vega/Model/VehicleFeature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vega.Model
+{
+    [Table("VehicleFeature")]
+    public class VehicleFeature
+    {
+
+        public int VehicleId { get; set; }
+        public int FeatureId { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Feature Feature { get; set; }
+        public ICollection<VehicleFeature> Features { get; set; }
+        public VehicleFeature()
+        {
+            Features = new Collection<VehicleFeature>();
+        }
+    }
+}
